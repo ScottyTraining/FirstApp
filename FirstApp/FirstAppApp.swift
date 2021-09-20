58a41b72515546803b41cd3a8385c287bf4fbb36
Scotty Test <scottmvau@hotmail.com>
2021-09-20 19:31:37 +1000

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by MrMacLappy on 20/9/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
